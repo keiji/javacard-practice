a67dd3c9dcdbfda7e0dbe04d60f629cf469622f7
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-08-23 00:29:13 +0900

    Setting for JavaCard applet development.

--- javacard-applet/build.gradle.kts
+++ javacard-applet/build.gradle.kts
@@ -10,8 +10,23 @@ repositories {
 }
 
 dependencies {
+    implementation(
+        fileTree(
+            mapOf(
+                "dir" to "$rootDir/libs/",
+                "include" to listOf("*.jar"),
+            )
+        )
+    )
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.withType(JavaCompile::class.java) {
+    options.compilerArgs = mutableListOf("-g")
+    targetCompatibility = JavaVersion.VERSION_1_7.toString()
+    sourceCompatibility = JavaVersion.VERSION_1_7.toString()
 }
 
 tasks.getByName<Test>("test") {
