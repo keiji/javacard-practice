64151a094e99dd14e78a1c34b0691e0842219a35
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-08-25 01:08:48 +0900

    Change applet converting process from a shellscropt to a gradle task.

--- javacard-applet/build.gradle.kts
+++ javacard-applet/build.gradle.kts
@@ -1,3 +1,24 @@
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.*
+
+fun getProperty(file: String): Properties {
+    val localProperties = File(file)
+    if (!localProperties.exists() || !localProperties.isFile) {
+        error("File ${file} not found")
+    }
+
+    val properties = Properties()
+    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
+        properties.load(reader)
+    }
+
+    return properties
+}
+
+val props = getProperty("local.properties")
+
 plugins {
     id("java")
 }
@@ -27,6 +48,36 @@ tasks.withType(JavaCompile::class.java) {
     options.compilerArgs = mutableListOf("-g")
     targetCompatibility = JavaVersion.VERSION_1_7.toString()
     sourceCompatibility = JavaVersion.VERSION_1_7.toString()
+}
+
+val javaCardSdkLibPath = props["javacardsdk.path"]
+val appletPackageName = "dev.keiji.javacard.applet"
+val appletPackageAid = "0x0F:02:03:04:05"
+val appletVersion = "1.0"
+val appletAid = "0x0F:02:03:04:05:06:07:03:02"
+val appletClassName = "dev.keiji.javacard.applet.App"
+val appletTargetJavaCardOsVersion = "3.0.4"
+
+tasks.create("convertToCap", JavaExec::class) {
+    classpath = files(
+        "${javaCardSdkLibPath}\\asm-8.0.1.jar",
+        "${javaCardSdkLibPath}\\commons-cli-1.4.jar",
+        "${javaCardSdkLibPath}\\commons-logging-1.2-9f99a00.jar",
+        "${javaCardSdkLibPath}\\jctasks_tools.jar",
+        "${javaCardSdkLibPath}\\json.jar",
+        "${javaCardSdkLibPath}\\tools.jar",
+        "${javaCardSdkLibPath}\\api_classic-3.0.4.jar",
+        "${javaCardSdkLibPath}\\api_classic_annotations-3.0.4.jar",
+    )
+    main = "com.sun.javacard.converter.Main"
+    args = listOf(
+        appletPackageName, appletPackageAid, appletVersion,
+        "-classdir", "${buildDir}/classes/java/main",
+        "-applet", appletAid, appletClassName,
+        "-d", "${buildDir}/applet",
+        "-target", appletTargetJavaCardOsVersion,
+        "-i"
+    )
 }
 
 tasks.getByName<Test>("test") {
