5519ce008a8582d46b5c432eebcc501c6f3935e4
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-09-03 00:49:31 +0900

    Implement Verify command.

--- javacard-applet/src/main/java/dev/keiji/javacard/applet/App.java
+++ javacard-applet/src/main/java/dev/keiji/javacard/applet/App.java
@@ -6,37 +6,58 @@ public class App extends Applet {
     private static final byte APDU_SELECT_CLA = 0x00;
     private static final byte APDU_SELECT_INS = (byte) 0xA4;
 
-    private App() {
-        register();
-    }
+    private static final byte INS_VERIFY = 0x20;
+
+    private static final byte[] DEFAULT_PIN = new byte[]{0x04, 0x05, 0x06, 0x07};
 
     public static void install(byte[] bArray, short bOffset, byte bLength) throws ISOException {
         App theApplet = new App();
+        theApplet.register();
+    }
+
+    private OwnerPIN ownerPin;
+    private boolean pinLocked = true;
+
+    private App() {
+        initialize();
+    }
+
+    private void initialize() {
+
+        // Initialize PIN
+        ownerPin = new OwnerPIN((byte) 3, (byte) 4);
+        ownerPin.update(DEFAULT_PIN, (short) 0, (byte) DEFAULT_PIN.length);
     }
 
     @Override
     public void process(APDU apdu) throws ISOException {
-        byte[] buffer = apdu.getBuffer();
-        byte cla = buffer[ISO7816.OFFSET_CLA];
-        byte ins = buffer[ISO7816.OFFSET_INS];
-        byte p1 = buffer[ISO7816.OFFSET_P1];
-        byte p2 = buffer[ISO7816.OFFSET_P2];
+        byte cla = apdu.getBuffer()[ISO7816.OFFSET_CLA];
+        byte ins = apdu.getBuffer()[ISO7816.OFFSET_INS];
 
         if (cla == APDU_SELECT_CLA && ins == APDU_SELECT_INS) {
             return;
         }
 
-        short le = apdu.setOutgoing();
-        if (le < 2) {
-            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+        switch (ins) {
+            case INS_VERIFY:
+                processVerify(apdu);
+                break;
+            default:
+                ISOException.throwIt(ISO7816.SW_UNKNOWN);
+                break;
         }
+    }
 
-        apdu.setOutgoingLength(le);
+    private static final byte SW1_MEMORY_CHANGED = 0x63;
 
-        for (byte i = 0; i < le; i++) {
-            buffer[i] = (byte) (buffer[i] ^ (byte) 0xFF);
+    private void processVerify(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+
+        short lc = apdu.setIncomingAndReceive();
+        pinLocked = !ownerPin.check(buffer, ISO7816.OFFSET_CDATA, (byte) lc);
+        if (pinLocked) {
+            short statusWord = (short) ((SW1_MEMORY_CHANGED << 8) | ownerPin.getTriesRemaining());
+            ISOException.throwIt(statusWord);
         }
-
-        apdu.sendBytes((short) 0, le);
     }
 }
